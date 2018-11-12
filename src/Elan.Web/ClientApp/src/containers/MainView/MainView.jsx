ddc1772c9534014b5e932ed995177fcd00daaecb
mowerczuk <maciej.owerczuk@gmail.com>
2018-11-12 12:18:40 +0100

    removed # from url address, added preventDefault on form submits

--- src/Elan.Web/ClientApp/src/containers/MainView/MainView.jsx
+++ src/Elan.Web/ClientApp/src/containers/MainView/MainView.jsx
@@ -2,19 +2,18 @@ import React, { Component } from 'react';
 import Login from '../../components/Login/Login';
 import Register from '../../components/Register/Register';
 import App from '../App/App';
-import { Route, Router } from "react-router-dom";
+import { Route, BrowserRouter as Router, Link } from "react-router-dom";
 import { createHashHistory } from 'history';
 import './MainView.css';
 
 export default class MainView extends Component {
   constructor(props){
     super(props);
-    this.history = createHashHistory();
   }
 
   render() {
     return (
-      <Router history={this.history}>
+      <Router>
         <div className="main-view">
             <Route exact path="/" render={() => (<p>Login. App in /#/app url</p>)}/>
             <Route exact path="/login" render={() => <Login /> }/>
