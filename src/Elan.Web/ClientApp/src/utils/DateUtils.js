5e19711d04e76c41349f400ca0977ca2b400b666
Kamil Kurys <kkurys@10g.pl>
2018-12-07 00:05:04 +0100

    fixing chat date format

--- src/Elan.Web/ClientApp/src/utils/DateUtils.js
+++ src/Elan.Web/ClientApp/src/utils/DateUtils.js
@@ -1,6 +1,5 @@
 export const getFormattedDate = (date) => {
-    date = String(date).split('T');
-    var days = String(date[0]).split('-');
-    var hours = String(date[1]).split(':');
-    return date[0] + " " + parseInt(hours[0]) + ":" + parseInt(hours[1]) + ":" + parseInt(hours[2]);
+    debugger;
+    date = new Date(date);
+    return date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear() + ' ' + date.getHours() + ':' + date.getMinutes();
 }
